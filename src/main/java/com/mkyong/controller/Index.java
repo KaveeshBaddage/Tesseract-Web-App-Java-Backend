72af264d31e1ba18acd3e39ab3319c36cfac08cd
Kaveesha Baddage <MPL_KaveeshaB@millenniumitesp.com>
2018-06-13 22:50:38 +0530

    Add new plugin to build jar

---
+++ src/main/java/com/mkyong/controller/Index.java
@@ -0,0 +1,10 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+    @RequestMapping("/")
+    String index(){
+        return "index.html";
+    }
+}
